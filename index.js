a0f909fd49bd2241e5b2f971415e8871e1ea1043
Alejandro Cruz Hernández <alejandrocruz309@aragon.unam.mx>
2020-05-11 23:11:38 -0500

    Creacion del index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send("Mi primer server corriendo en http://localhost:3000")
+})
+
+app.listen(3000)
